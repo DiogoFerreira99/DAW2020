94114a95d873ead0f082b65e24f0f299e29da97f
DiogoFerreira99 <diogoaraujoferreira4@gmail.com>
2020-11-11 12:46:40 +0000

    tpc4

---
+++ Documents/DAW/T4/server2.js
@@ -0,0 +1,50 @@
+var http = require ("http");
+var fs = require('fs')
+var aux=require ("./mymod.js")
+var url = require('url')
+
+http.createServer(function(req, res){
+    console.log(req.method + " " + req.url + " " + aux.myDateTime())  
+
+    var num = req.url.split("/")[req.url.split("/").length-1]  // Para suportar abrir mais do que um ficheiro
+    console.log(num)
+
+
+    if(req.url.match(/\/$/)){
+        console.log(req.url)
+        fs.readFile("site/index.html", function(err, data){
+            if(err){
+                res.writeHead (404,{'Content-Type': "text/html"});
+                res.write("<p>URL nao esta correto</p>")
+                res.end ()
+            }else{
+            res.writeHead (200,{'Content-Type': " text/html"});
+            res.write(data)
+            res.end ()
+            }
+        })
+
+    }else if(req.url.match(/\/arq\/[0-9]{1,3}$/)){
+        fs.readFile("site/arq" + num + ".html", function(err, data){
+            if(err){
+                res.writeHead (404,{'Content-Type': "text/html"});
+                res.write("<p>URL nao esta correto</p>")
+                res.end ()
+            }else{
+            res.writeHead (200,{'Content-Type': " text/html"});
+            res.write(data)
+            res.end ()
+            }
+        })
+
+    }
+    else{
+        res.writeHead (404,{'Content-Type': "text/html"});
+        res.write("<p>URL nao esta correto</p>")
+        res.end ()
+    }
+
+    
+}).listen(7777);
+
+console.log('Servidor a correr na porta 7777...')
